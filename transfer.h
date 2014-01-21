6c253a78e5f2c617821c2bb70a60fdec070a669e
theqlabs <q@crypto.com>
2014-01-20 22:45:35 -0500

    Big change to code, things are simpler than they first appeared.

--- transfer.h
+++ transfer.h
@@ -1,3 +1,14 @@
+/*
+ * ALSA PCM interface for the TI DAVINCI processor
+ *
+ * Author:      Vladimir Barinov, <vbarinov@embeddedalley.com>
+ * Copyright:   (C) 2007 MontaVista Software, Inc., <source@mvista.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
 #ifndef _DAVINCI_PCM_H
 #define _DAVINCI_PCM_H
 
@@ -5,8 +16,7 @@
 /* #include <linux/platform_data/davinci_asp.h> */
 #include <linux/platform_data/edma.h>
 
-/* struct davinci_pcm_dma_params { */
-struct dma_params {
+struct davinci_pcm_dma_params {
 	int channel;						/* sync dma channel ID */
 	unsigned short acnt;
 	dma_addr_t dma_addr;					/* device physical address for DMA */
@@ -15,16 +25,11 @@ struct dma_params {
 	enum dma_event_q asp_chan_q;				/* event queue number for ASP channel */
 	enum dma_event_q ram_chan_q;				/* event queue number for RAM channel */
 	unsigned char data_type;				/* xfer data type */
-	//unsigned char convert_mono_stereo;
+	unsigned char convert_mono_stereo;
 	unsigned int fifo_level;
 };
 
-/*
 int davinci_soc_platform_register(struct device *dev);
 void davinci_soc_platform_unregister(struct device *dev);
-*/
 
-int transfer_platform_register(struct device *dev);
-void transfer_platform_unregister(struct device *dev);
 #endif
-
