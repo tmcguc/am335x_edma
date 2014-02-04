2140b8a484a6538a03fb98e9b4966d90a286919d
theqlabs <q@theqlabs.com>
2014-02-04 18:35:34 +0000

    Mighty_DMA.c - working on EDMA transferring.

--- mighty_dma.c
+++ mighty_dma.c
@@ -26,48 +26,58 @@
 #include <linux/platform_device.h>		/* Register/Unregister Platform Drivers */
 #include <linux/platform_data/edma.h>   /* edmacc_param type */
 
-#define PINGCHAN		20
-#define PONGCHAN		64
+#include "virt-dma.h"
+
+#define DEBUG		1	
+#define PINGCHAN	20
+#define PONGCHAN	64
 #define DMA_POOL_SIZE	4096
 
 #define EDMA_CTLRS	2
 #define EDMA_CHANS	32
 
 /* Max of 16 segments per channel to conserve PaRAM slots */
-#define MAX_NR_SG			16
+#define MAX_NR_SG		16
 #define EDMA_MAX_SLOTS		MAX_NR_SG
 #define EDMA_DESCRIPTORS	16
 
 /* from d/d/edma.c */
- struct edma_desc {
+struct edma_desc {
 	//struct virt_dma_desc	vdesc;
 	//struct list_head		node;
-	int						absync;
-	int						pset_nr;
+	int				absync;
+	int				pset_nr;
 	struct edmacc_param		pset[0];
 };
 
 struct edma_cc;
 
 struct edma_chan {
-	//struct virt_dma_chan		vchan;
-	//struct list_head			node;
-	struct edma_desc			*edesc;
-	struct edma_cc				*ecc;
-	int							ch_num;
-	bool						alloced;
-	int							slot[EDMA_MAX_SLOTS];
-	//struct dma_slave_config		cfg;
+	struct virt_dma_chan		vchan;
+	//struct list_head		node;
+	struct edma_desc		*edesc;
+	struct edma_cc			*ecc;
+	int				ch_num;
+	bool				alloced;
+	int				slot[EDMA_MAX_SLOTS];
+	//struct dma_slave_config	cfg;
 	//struct dmaengine_chan_caps	caps;
 };
 
 struct edma_cc {
-	int						ctlr;
+	int				ctlr;
 	//struct dma_device		dma_slave;
 	struct edma_chan		slave_chans[EDMA_CHANS];
-	int						num_slave_chans;
-	int						slot_20;			/* TESTING */
-	int 					slot_17;			/* FOR SPI TRIGGER */
+	int				num_slave_chans;
+	int				slot_20;			/* TESTING */
+	int 				slot_17;			/* FOR SPI TRIGGER */
+	
+	struct ebic_dmac		*dmac_a;
+	struct ebic_dmac		*dmac_b;
+
+	struct mem_addr			*kmem_addr_a;		/* Memory Address returned from dma_pool_alloc */
+	struct mem_addr			*kmem_addr_b;
+
 };
 
 
@@ -80,18 +90,16 @@ struct mem_addr {
 
 /* EBIC DMA Channel Structure */
 struct ebic_dmac {
-
-	struct mem_addr		*kmem_addr;		/* Memory Address returned from dma_pool_alloc */
-	dma_addr_t			dma_handle;		/* DMA Handle set by dma_pool_alloc */
+	dma_addr_t		dma_handle;		/* DMA Handle set by dma_pool_alloc */
 
 	struct edmacc_param	*dma_test_params;
-	//struct edmacc_param	*pingset;
-	//struct edmacc_param	*pongset;
+	struct edmacc_param	*pingset;
+	struct edmacc_param	*pongset;
 
 	spinlock_t		lock;
-	int				dma_ch;
-	int				dma_link[2];
-	int				slot;
+	int			dma_ch;
+	int			dma_link[2];
+	int			slot;
 };
 
 static struct dma_pool *pool_a;				/* Required for dma_pool_create */
@@ -122,6 +130,10 @@ static struct edmacc_param pongset = {
 };
 */
 
+static inline struct edma_chan *to_edma_chan(struct dma_chan *c) {
+	return container_of(c, struct edma_chan, vchan.chan);
+}
+
 static void dma_callback(unsigned link, u16 ch_status, void *data) {
 
 	printk(KERN_INFO "EDMA transfer test: link=%d, status=0x%x\n", link, ch_status);
@@ -130,12 +142,40 @@ static void dma_callback(unsigned link, u16 ch_status, void *data) {
 		return;
 }
 
-//static int edma_probe(struct ebic_dmac *edev) {
+/* 
+* Allocate CHANNELS
+*
+*  Here's the skinny: Allocating dma_channel (master), exception handler if RET < 0 then allocation failed
+*	then print to kernel debug (dmesg | tail) then I'm setting RET equal to the struct ebic_dmac 
+* 	field dma_chan for later use.
+*
+*/
+static int edma_alloc_chan_resources(struct dma_chan *chan) {
+
+	struct edma_chan *echan = to_edma_chan(chan);
+	//struct device *dev = chan->device->dev;
+	int a_ch_num;
+	int ret;
+
+	a_ch_num = edma_alloc_channel(echan->ch_num, dma_callback, chan, EVENTQ_0);
+
+	if (a_ch_num < 0) {
+		printk(KERN_INFO "allocating channel for DMA failed\n");
+		ret = -EINVAL;
+		return ret;
+	}
+
+	echan->alloced = true;
+	echan->slot[0] = a_ch_num;
+#ifdef DEBUG
+	printk(KERN_INFO "ret=%d, dma_ch=%d\n", ret, echan->slot[0]);
+#endif
+	return 0;
+}
+
 static int edma_probe(struct platform_device *pdev) {
 
-	int dma_channel = PINGCHAN;
-	//int ctlr;
-	int ret;
+	struct edma_cc *ecc;
 
 	struct ebic_dmac *dmac_a;
 	struct ebic_dmac *dmac_b;
@@ -143,67 +183,73 @@ static int edma_probe(struct platform_device *pdev) {
 	struct mem_addr *kmem_addr_a;
 	struct mem_addr *kmem_addr_b;
 
-	//struct device *dev_a;			/* Where are these set? */
-	//struct device *dev_b;
-
+#ifdef DEBUG
 	printk(KERN_INFO "Entering edma_probe function\n");
 	printk(KERN_INFO "Allocating Memory for DMA\n");
+#endif
 
-	/*
-	 *
-	 * 	MEMORY AND DMA_POOL ALLOCATION
-	 *
-	 */
-	/* Allocate mem for dmac structs */
-	dmac_a = kzalloc(sizeof(struct ebic_dmac), GFP_KERNEL);
-	dmac_b = kzalloc(sizeof(struct ebic_dmac), GFP_KERNEL);
-	
-	if (!dmac_a || dmac_b) {
-		printk(KERN_ERR "%s: failed to alloc mem for struct ebic_dmac\n", __func__);
+	ecc = devm_kzalloc(&pdev->dev, sizeof(*ecc), GFP_KERNEL);
+	if (!ecc) {
+		printk(KERN_INFO "ecc memory can not be allocated\n");
 		return -ENOMEM;
 	}
 
-	/* Create a dma_pool */
+	ecc->ctlr = pdev->id;
+	
+	dmac_a = kzalloc(sizeof(struct ebic_dmac), GFP_KERNEL);
+	dmac_b = kzalloc(sizeof(struct ebic_dmac), GFP_KERNEL);
+
+
+	if (!dmac_a) {
+		printk(KERN_ERR "%s: failed to alloc mem for dmac_a\n", __func__);
+		return -ENOMEM;
+	}
+
+	if (!dmac_b) {
+		printk(KERN_ERR "%s: failed to alloc mem for dmac_b\n", __func__);
+		return -ENOMEM;
+	}
+
 	pool_a = dma_pool_create("pool_a memtest", NULL, DMA_POOL_SIZE, 16, 0);
 	pool_b = dma_pool_create("pool_b memtest", NULL, DMA_POOL_SIZE, 16, 0);
 	
-	if (!pool_a || pool_b) {
-		printk(KERN_ERR "%s: failed to create pool\n", __func__);
+
+	if (!pool_a) {
+		printk(KERN_ERR "%s: failed to create pool_a\n", __func__);
+		return -ENOMEM;
+	}
+
+	if (!pool_b) {
+		printk(KERN_ERR "%s: failed to create pool_b\n", __func__);
 		return -ENOMEM;
 	}
 
 
-	/* Allocate memory from a dma_pool */
 	kmem_addr_a = dma_pool_alloc(pool_a, GFP_ATOMIC, &dmac_a->dma_handle);
 	kmem_addr_b = dma_pool_alloc(pool_b, GFP_ATOMIC, &dmac_b->dma_handle);
 
-	if (!kmem_addr_a || kmem_addr_b) {
-		printk(KERN_ERR "%s: failed to allocate memory for dma_pool\n", __func__);
+
+	if (!kmem_addr_a) {
+		printk(KERN_ERR "%s: failed to allocate memory for kmem_addr_a\n", __func__);
 		return -ENOMEM;
 	}
-
-	/* DEBUG: Prints values for own sanity */
-	printk(KERN_INFO "dma_handle_a: 0x%08x dma_handle_b: 0x%08x", dmac_a->dma_handle, dmac_b->dma_handle);
-	printk(KERN_INFO "kmem_addr_a:%p, kmem_addr_b:%p", &kmem_addr_a, &kmem_addr_b);
-
-	//printk(KERN_INFO "kmem_addr_a->src_addr: %08x, kmem_addr_b->src_addr: %08x", kmem_addr_a->src_addr, kmem_addr_b->dst_addr);
-	//printk(KERN_INFO "kmem_addr_a->dst_addr: %08x, kmem_addr_b->dst_addr: %08x", kmem_addr_a->dst_addr, kmem_addr_b->dst_addr);
-
-	/* 
-	* Allocate CHANNELS
-	*
-	*  Here's the skinny: Allocating dma_channel (master), exception handler if RET < 0 then allocation failed
-	*	then print to kernel debug (dmesg | tail) then I'm setting RET equal to the struct ebic_dmac 
-	* 	field dma_chan for later use.
-	*
-	*/
-	ret = edma_alloc_channel(dma_channel, dma_callback, pdev, EVENTQ_0);
-	if (ret < 0) {
-		printk(KERN_INFO "allocating channel for DMA failed\n");
-		return ret;
+	if (!kmem_addr_b) {
+		printk(KERN_ERR "%s: failed to allocate memory for kmem_addr_b\n", __func__);
+		return -ENOMEM;
 	}
-	edev->dma_ch = ret;
-	printk(KERN_INFO "ret=%d, dma_ch=%d\n", ret, edev->dma_ch);
+s
+
+	//ecc->kmem_addr_a = kmem_addr_a;
+	//ecc->kmem_addr_b = kmem_addr_b;
+
+#ifdef DEBUG
+	printk(KERN_INFO "dma_handle_a: 0x%08x dma_handle_b: 0x%08x", ecc->dmac_a->dma_handle, ecc->dmac_b->dma_handle);
+	printk(KERN_INFO "kmem_addr_a:%p, kmem_addr_b:%p", &ecc->kmem_addr_a, &ecc->kmem_addr_b);
+	printk(KERN_INFO "kmem_addr_a->src_addr: %08x, kmem_addr_b->src_addr: %08x", ecc->kmem_addr_a->src_addr, ecc->kmem_addr_b->dst_addr);
+	printk(KERN_INFO "kmem_addr_a->dst_addr: %08x, kmem_addr_b->dst_addr: %08x", ecc->kmem_addr_a->dst_addr, ecc->kmem_addr_b->dst_addr);
+#endif
+	// Run the EDMA Allocate Channel function
+	//edma_alloc_chan_resources;
 
 	/* 
 	* Allocate SLOTS
@@ -211,6 +257,7 @@ static int edma_probe(struct platform_device *pdev) {
 	*  Here's the skinny: For testing I just need a single secondary channel to transfer data and test this shit
 	*
 	*/
+	/*
 	ret = edma_alloc_slot(EDMA_CTLR(edev->dma_ch), EDMA_SLOT_ANY);
 	if (ret < 0) {
 		printk(KERN_INFO "allocating slot for DMA failed\n");
@@ -229,7 +276,11 @@ static int edma_probe(struct platform_device *pdev) {
 		printk(KERN_INFO "edma_start failed\n");
 		return ret;
 	}
-	
+	*/
+
+	//ecc->dmac_a = dmac_a;
+	//ecc->dmac_b = dmac_b;
+
 	return 0;
 }
 
@@ -238,6 +289,20 @@ static int edma_remove(struct platform_device *pdev) {
 	struct device *dev = &pdev->dev;
 	struct edma_cc *ecc = dev_get_drvdata(dev);						/* return dev->driver_data; */
 
+	printk(KERN_INFO "Freeing all dma_pool memory\n");
+
+	dma_pool_free(pool_a, &ecc->kmem_addr_a, ecc->dmac_a->dma_handle);
+	dma_pool_free(pool_b, &ecc->kmem_addr_b, ecc->dmac_b->dma_handle);
+	kfree(ecc->dmac_a);
+	kfree(ecc->dmac_b);
+	dma_pool_destroy(pool_a);
+	dma_pool_destroy(pool_b);
+
+
+	printk(KERN_INFO "dmac_a->dma_chan=%d\n", ecc->dmac_a->dma_ch);
+	printk(KERN_INFO "dmac_b->dma_chan%d\n", ecc->dmac_b->dma_ch);
+	// Disable EDMA
+	edma_stop(ecc->dmac_a->dma_ch);
 	/* Moved from edma_remove */
 	edma_free_slot(ecc->slot_20); 
 
@@ -281,28 +346,6 @@ out:
 subsys_initcall(dma_init);
 
 static void __exit dma_exit(void) {
-
-	struct ebic_dmac *dmac_a;
-	struct ebic_dmac *dmac_b;
-
-	struct mem_addr *kmem_addr_a;
-	struct mem_addr *kmem_addr_b;
-
-	/* Free memory from dma_pool */
-	printk(KERN_INFO "Freeing all dma_pool memory\n");
-
-	dma_pool_free(pool_a, &kmem_addr_a, dmac_a->dma_handle);
-	dma_pool_free(pool_b, &kmem_addr_b, dmac_b->dma_handle);
-	kfree(dmac_a);
-	kfree(dmac_b);
-	dma_pool_destroy(pool_a);
-	dma_pool_destroy(pool_b);
-
-
-	printk(KERN_INFO "dmac_a->dma_chan=%d\n", dmac_a->dma_ch);
-	printk(KERN_INFO "dmac_b->dma_chan%d\n", dmac_b->dma_ch);
-	// Disable EDMA
-	edma_stop(dmac_a->dma_ch);
 
 	printk(KERN_INFO "Unregistering Device and Driver\n");
 
