d8d09acfe8339443d62b0cebfca7099f3417f8a0
theqlabs <q@crypto.com>
2014-03-11 02:23:27 +0000

    Adding TI EDMA Sample Application for reference

--- testmodule.sh
+++ testmodule.sh
@@ -1,6 +1,6 @@
-sudo cp ./mighty_dma.ko /lib/modules/3.8.13-bone28/kernel/drivers/dma/
+sudo cp ./mighty_dma.ko /lib/modules/3.8.13-bone40/build/drivers/dma/
 sudo depmod -a 
 sudo modprobe mighty_dma
 sleep 5
-#sudo modprobe -r mighty_dma
+sudo modprobe -r mighty_dma
 dmesg | tail
