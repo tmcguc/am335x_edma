8eba20584e56980841db49ace7c6c9fe10460659
theqlabs <q@crypto.com>
2014-03-21 17:09:48 +0000

    ebic DIR ... driver is working, terrence about to config for ABSYNC

--- ebic/mighty_ebic/mighty_dma.c
+++ ebic/mighty_ebic/mighty_dma.c
@@ -8,6 +8,8 @@
  *   Andrew Righter <q@crypto.com>
  *	for Ephemeron-Labs, Inc.
  *
+ *   TODO - Enable users to easily switch between ASYNC and ABSYNC modes (edma_set_transfer_parameters)
+ *   TODO - Have users dynamically set OPT values (we're intending to do it from IOCTL in Userland for our own use) 
  */
 
 #include <linux/module.h>
@@ -36,6 +38,16 @@
 //#undef EBIC
 #define EBIC
 
+/* Which Sync Mode Do you Require? */
+/* TODO */
+
+/* How many bytes do you wish to transfer, in total? */
+#ifdef EBIC
+#define MAX_DMA_TRANSFER_IN_BYTES   (32)	/* EBIC test values (ACNT*BCNT*CCNT) */
+#else
+#define MAX_DMA_TRANSFER_IN_BYTES   (32768)     /* Original TI EDMA Sample Values */
+#endif
+
 /* useful defines for debug output */
 #ifdef EDMA3_DEBUG
 #define DMA_PRINTK(ARGS...)  printk(KERN_INFO "<%s>: ",__FUNCTION__);printk(ARGS)
@@ -47,12 +59,6 @@
 #define DMA_FN_OUT
 #endif
 
-#ifdef EBIC
-#define MAX_DMA_TRANSFER_IN_BYTES   (32)	/* EBIC test values (ACNT*BCNT*CCNT) */
-#else
-#define MAX_DMA_TRANSFER_IN_BYTES   (32768)   /* Original TI EDMA Sample Values */
-#endif
-
 #define STATIC_SHIFT                3
 #define TCINTEN_SHIFT               20
 #define ITCINTEN_SHIFT              21
@@ -62,9 +68,7 @@
 static volatile int irqraised1 = 0;
 static volatile int irqraised2 = 0;
 
-//int edma3_memtomemcpytest_dma(int acnt, int bcnt, int ccnt, int sync_mode, int event_queue);
 int edma3_memtomemcpytest_dma_link(int acnt, int bcnt, int ccnt, int sync_mode, int event_queue);
-//int edma3_memtomemcpytest_dma_chain(int acnt, int bcnt, int ccnt, int sync_mode, int event_queue);
 
 dma_addr_t dmaphyssrc1 = 0;
 dma_addr_t dmaphyssrc2 = 0;
@@ -76,7 +80,7 @@ char *dmabufsrc2 = NULL;
 char *dmabufdest1 = NULL;
 char *dmabufdest2 = NULL;
 
-/* EBIC values AB-Synchronized Transfer */
+/* EBIC values */
 #ifdef EBIC
 static int acnt = 4;		/* FIFO width (32-bit) */
 static int bcnt = 8;		/* FIFO depth */
@@ -113,8 +117,11 @@ static int __init edma_test_init(void)
 {
 	int result = 0;
 	int iterations = 0;
+	#ifdef EBIC
 	int numTCs = 2;
-	//int numTCs = 4;
+	#else	
+	int numTCs = 4;
+	#endif
 	int modes = 2;
 	int i,j;
 
@@ -127,49 +134,39 @@ static int __init edma_test_init(void)
 	 * dmabufsrc1 = CPU-viewed address
 	 */
 
-	dmabufsrc1 = dma_alloc_coherent (NULL, MAX_DMA_TRANSFER_IN_BYTES,
-					&dmaphyssrc1, 0);
+	dmabufsrc1 = dma_alloc_coherent (NULL, MAX_DMA_TRANSFER_IN_BYTES, &dmaphyssrc1, 0);
 	DMA_PRINTK( "\nSRC1:\t%x", dmaphyssrc1);
 	if (!dmabufsrc1) {
 		DMA_PRINTK ("dma_alloc_coherent failed for dmaphyssrc1\n");
 		return -ENOMEM;
 	}
 
-	dmabufdest1 = dma_alloc_coherent (NULL, MAX_DMA_TRANSFER_IN_BYTES,
-						&dmaphysdest1, 0);
+	dmabufdest1 = dma_alloc_coherent (NULL, MAX_DMA_TRANSFER_IN_BYTES, &dmaphysdest1, 0);
 	DMA_PRINTK( "\nDST1:\t%x", dmaphysdest1);
 	if (!dmabufdest1) {
 		DMA_PRINTK("dma_alloc_coherent failed for dmaphysdest1\n");
-		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc1,
-				dmaphyssrc1);
+		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc1, dmaphyssrc1);
 		return -ENOMEM;
 	}
 
-	dmabufsrc2 = dma_alloc_coherent (NULL, MAX_DMA_TRANSFER_IN_BYTES,
-					&dmaphyssrc2, 0);
+	dmabufsrc2 = dma_alloc_coherent (NULL, MAX_DMA_TRANSFER_IN_BYTES, &dmaphyssrc2, 0);
 	DMA_PRINTK( "\nSRC2:\t%x", dmaphyssrc2);
 	if (!dmabufsrc2) {
 		DMA_PRINTK ("dma_alloc_coherent failed for dmaphyssrc2\n");
 
-		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc1,
-				dmaphyssrc1);
-		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufdest1,
-				dmaphysdest1);
+		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc1, dmaphyssrc1);
+		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufdest1, dmaphysdest1);
 		return -ENOMEM;
 	}
 
-	dmabufdest2 = dma_alloc_coherent (NULL, MAX_DMA_TRANSFER_IN_BYTES,
-					&dmaphysdest2, 0);
+	dmabufdest2 = dma_alloc_coherent (NULL, MAX_DMA_TRANSFER_IN_BYTES, &dmaphysdest2, 0);
 	DMA_PRINTK( "\nDST2:\t%x", dmaphysdest2);
 	if (!dmabufdest2) {
 		DMA_PRINTK ("dma_alloc_coherent failed for dmaphysdest2\n");
 
-		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc1,
-				dmaphyssrc1);
-		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufdest1,
-				dmaphysdest1);
-		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc2,
-				dmaphyssrc2);
+		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc1, dmaphyssrc1);
+		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufdest1, dmaphysdest1);
+		dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc2, dmaphyssrc2);
 		return -ENOMEM;
 	}
 
@@ -204,125 +201,15 @@ static int __init edma_test_init(void)
 void edma_test_exit(void)
 {
 	dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc1, dmaphyssrc1);
-	dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufdest1,
-			dmaphysdest1);
+	dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufdest1, dmaphysdest1);
 
 	dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufsrc2, dmaphyssrc2);
-	dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufdest2,
-			dmaphysdest2);
+	dma_free_coherent(NULL, MAX_DMA_TRANSFER_IN_BYTES, dmabufdest2, dmaphysdest2);
 
 	printk ("\nExiting edma_test module\n");
 }
 
-
-
-/* DMA Channel, Mem-2-Mem Copy, ASYNC Mode, INCR Mode */
-
-int edma3_memtomemcpytest_dma (int acnt, int bcnt, int ccnt, int sync_mode, int event_queue)
-{
-	int result = 0;
-	unsigned int dma_ch = 0;
-	int i;
-	int count = 0;
-	unsigned int Istestpassed = 0u;
-	unsigned int numenabled = 0;
-	unsigned int BRCnt = 0;
-	int srcbidx = 0;
-	int desbidx = 0;
-	int srccidx = 0;
-	int descidx = 0;
-	struct edmacc_param param_set;
-
-	/* Initalize source and destination buffers */
-	for (count = 0u; count < (acnt*bcnt*ccnt); count++) {
-		dmabufsrc1[count] = 'A' + (count % 26);
-		dmabufdest1[count] = 0;
-	}
-
-	/* Set B count reload as B count. */
-	BRCnt = bcnt;
-
-	/* Setting up the SRC/DES Index */
-	srcbidx = acnt;
-	desbidx = acnt;
-
-	/* A Sync Transfer Mode */
-	srccidx = acnt;
-	descidx = acnt;
-
-	result = edma_alloc_channel (EDMA_CHANNEL_ANY, callback1, NULL, event_queue);
-
-	if (result < 0) {
-		DMA_PRINTK ("\nedma3_memtomemcpytest_dma::edma_alloc_channel failed for dma_ch, error:%d\n", result);
-		return result;
-	}
-
-	dma_ch = result;
-	edma_set_src (dma_ch, (unsigned long)(dmaphyssrc1), INCR, W8BIT);
-	edma_set_dest (dma_ch, (unsigned long)(dmaphysdest1), INCR, W8BIT);
-	edma_set_src_index (dma_ch, srcbidx, srccidx);
-	edma_set_dest_index (dma_ch, desbidx, descidx);
-	/* A Sync Transfer Mode */
-	edma_set_transfer_params (dma_ch, acnt, bcnt, ccnt, BRCnt, ASYNC);
-
-	/* Enable the Interrupts on Channel 1 */
-	edma_read_slot (dma_ch, &param_set);
-	param_set.opt |= (1 << ITCINTEN_SHIFT);
-	param_set.opt |= (1 << TCINTEN_SHIFT);
-	param_set.opt |= EDMA_TCC(EDMA_CHAN_SLOT(dma_ch));
-	edma_write_slot (dma_ch, &param_set);
-
-	numenabled = bcnt * ccnt;
-
-	for (i = 0; i < numenabled; i++) {
-		irqraised1 = 0;
-
-		/*
-		 * Now enable the transfer as many times as calculated above.
-		 */
-		result = edma_start(dma_ch);
-		if (result != 0) {
-			DMA_PRINTK ("edma3_memtomemcpytest_dma: davinci_start_dma failed \n");
-			break;
-		}
-
-		/* Wait for the Completion ISR. */
-		while (irqraised1 == 0u);
-
-		/* Check the status of the completed transfer */
-		if (irqraised1 < 0) {
-			/* Some error occured, break from the FOR loop. */
-			DMA_PRINTK ("edma3_memtomemcpytest_dma: Event Miss Occured!!!\n");
-			break;
-		}
-	}
-
-	if (0 == result) {
-		for (i = 0; i < (acnt*bcnt*ccnt); i++) {
-			if (dmabufsrc1[i] != dmabufdest1[i]) {
-				DMA_PRINTK ("\n edma3_memtomemcpytest_dma: Data write-read matching failed at = %u\n",i);
-				Istestpassed = 0u;
-				break;
-			}
-		}
-		if (i == (acnt*bcnt*ccnt))
-			Istestpassed = 1u;
-
-		edma_stop(dma_ch);
-		edma_free_channel(dma_ch);
-	}
-
-	if (Istestpassed == 1u) {
-		DMA_PRINTK ("\nedma3_memtomemcpytest_dma: EDMA Data Transfer Successfull \n");
-	} else {
-		DMA_PRINTK ("\nedma3_memtomemcpytest_dma: EDMA Data Transfer Failed \n");
-	}
-
-	return result;
-}
-
 /* 2 DMA Channels Linked, Mem-2-Mem Copy, ASYNC Mode, INCR Mode */
-
 int edma3_memtomemcpytest_dma_link(int acnt, int bcnt, int ccnt, int sync_mode, int event_queue)
 {
 	int result = 0;
@@ -372,6 +259,7 @@ int edma3_memtomemcpytest_dma_link(int acnt, int bcnt, int ccnt, int sync_mode, 
 	edma_set_dest (dma_ch1, (unsigned long)(dmaphysdest1), INCR, W8BIT);
 	edma_set_src_index (dma_ch1, srcbidx, srccidx);
 	edma_set_dest_index (dma_ch1, desbidx, descidx);
+	/* TODO - Change to ABSYNC */
 	edma_set_transfer_params (dma_ch1, acnt, bcnt, ccnt, BRCnt, ASYNC);
 
 	/* Enable the Interrupts on Channel 1 */
@@ -394,6 +282,7 @@ int edma3_memtomemcpytest_dma_link(int acnt, int bcnt, int ccnt, int sync_mode, 
 	edma_set_dest (dma_ch2, (unsigned long)(dmaphysdest2), INCR, W8BIT);
 	edma_set_src_index (dma_ch2, srcbidx, srccidx);
 	edma_set_dest_index (dma_ch2, desbidx, descidx);
+	/* TODO - Change to ABSYNC */
 	edma_set_transfer_params (dma_ch2, acnt, bcnt, ccnt, BRCnt, ASYNC);
 
 	/* Enable the Interrupts on Channel 2 */
