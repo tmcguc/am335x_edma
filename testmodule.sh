1be636e3f4f0784765cb109013d825dfefd10e1a
Andrew Righter <q@crypto.com>
2014-03-25 17:41:07 -0400

    Reorganized DIR structure, much more sane now

--- testmodule.sh
+++
@@ -1,6 +0,0 @@
-sudo cp ./dma_drvr.ko /lib/modules/3.8.13-bone40/build/drivers/dma/
-sudo depmod -a 
-sudo insmod /lib/modules/3.8.13-bone40/build/drivers/dma/dma_drvr.ko
-sleep 5
-sudo rmmod /lib/modules/3.8.13-bone40/build/drivers/dma/dma_drvr.ko
-dmesg | tail
